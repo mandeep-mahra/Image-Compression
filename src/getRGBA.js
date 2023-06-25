a02ef20e0feba69fe87fd0626fca8c3874b61e6c
mandeep-mahra <mandeepmehara1@gmail.com>
2023-06-25 12:27:51 +0530

    first commit

---
+++ src/getRGBA.js
@@ -0,0 +1,26 @@
+const { createCanvas, Image, getImageData} = require('canvas')
+
+const img = new Image();
+img.src = "../images/random-dice.jpg"
+var canvas = createCanvas(img.width, img.height);
+const context = canvas.getContext('2d')
+context.drawImage(img, 0, 0);
+
+const width = img.width, height = img.height
+const arraySize = width * height * 4
+var imageData = context.getImageData(0, 0, width, height);
+var red = []
+var green = []
+var blue = []
+var alpha = []
+for(var i = 0; i<(img.height); i++){
+    for(var j = 0; j<(img.width); j++){
+        var index = (i*img.width + j) * 4;
+        red.push(imageData.data[index]);
+        green.push(imageData.data[index + 1]);
+        blue.push(imageData.data[index + 2]);
+        alpha.push(imageData.data[index + 3]);
+    }
+}
+
+module.exports = {red, green, blue, alpha};
---
+++ src/pcaCompression.js
@@ -0,0 +1,5 @@
+const pixels = require("./getRGBA")
+
+rgbImage = [pixels.red, pixels.green, pixels.blue, pixels.alpha];
+console.log(rgbImage)
+
