41e134730e1569d32f642d34f84539ad46124b7a
20bcs2915 <20bcs2915@cuchd.in>
2023-07-10 15:08:40 +0530

    test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,3 +1,13 @@
-test('1 + 1 = 2', () =>{
-    expect(1 + 1).toBe(2);
-})
+const KMeansMatrixConverter = require('../Backend/src/kMeans.js')
+
+console.log(typeof(KMeansMatrixConverter.arrayToMatrix([1,2,3,4,5,6,7,8,9], 3, 3)));
+console.log(typeof([ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]))
+test("Accepts an Array of values and returns a matrix of values with the given dimensions.", () => {
+    expect(JSON.stringify(KMeansMatrixConverter.arrayToMatrix([1,2,3,4,5,6,7,8,9], 3, 3)))
+    .toBe(JSON.stringify([ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]));
+});
+test("Accepts an Array of values and returns a matrix of values with the given dimensions.", () => {
+    expect(JSON.stringify(KMeansMatrixConverter.arrayToMatrix([1,2,3], 3, 1)))
+    .toBe(JSON.stringify([ [1, 2, 3] ]));
+});
+
