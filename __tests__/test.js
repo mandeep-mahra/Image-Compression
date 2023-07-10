2987efca5a4eff91d15d8b0ccd002b72c42bdcae
Arshdeep Singh <91689879+arsh73552@users.noreply.github.com>
2023-07-10 15:10:20 +0530

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,5 @@
 const KMeansMatrixConverter = require('../Backend/src/kMeans.js')
 
-console.log(typeof(KMeansMatrixConverter.arrayToMatrix([1,2,3,4,5,6,7,8,9], 3, 3)));
-console.log(typeof([ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]))
 test("Accepts an Array of values and returns a matrix of values with the given dimensions.", () => {
     expect(JSON.stringify(KMeansMatrixConverter.arrayToMatrix([1,2,3,4,5,6,7,8,9], 3, 3)))
     .toBe(JSON.stringify([ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]));
